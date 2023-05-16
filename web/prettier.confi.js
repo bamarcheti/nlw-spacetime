fd46c7a44461ecbac5fbbaa1eb66503f296f5b68
Bárbara Marcheti Fiorin <bfiorin@psgtecnologiaaplicada.com.br>
2023-05-16 11:48:44 -0400

    configurando tailwind e ESlint com Prettier

---
+++ web/prettier.confi.js
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: [require('prettier-plugin-tailwindcss')],
+  singleQuote: true,
+  tabWidth: 2,
+  endOfLine: "lf",
+  printWidth: 100,
+  trailingComma: "none"
+}
--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,14 +1,7 @@
-import Button from "@/components/Button";
-
 export default function Home() {
-    return (
-        <div>
-            <h1>Hello World</h1>
-            <Button title="Bárbara" />
-            <Button title="Laura" />
-            <Button title="Beatriz" />
-            <Button title="Priscila" />
-            <Button title="Paulo" />
-        </div>
-    );
+  return (
+    <div className="h-screen bg-zinc-950 text-zinc-50">
+      <h1 className="text-4xl font-bold">Sua cápsula do tempo</h1>
+    </div>
+  );
 }
