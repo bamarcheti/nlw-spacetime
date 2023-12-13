9969387b923ff3630832360e06ec5be4883f11c1
Bárbara Marcheti Fiorin <bfiorin@psgtecnologiaaplicada.com.br>
2023-12-13 17:19:36 -0400

    adicionando informações de autenticação e atualizado arquivo readme

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,9 @@
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree';
 import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session';
 import { StatusBar } from "expo-status-bar";
 import { styled } from 'nativewind';
+import { useEffect } from 'react';
 import { ImageBackground, Text, TouchableOpacity, View } from "react-native";
 import blurBg from './assets/bg-blur.png';
 import NlwLogo from './assets/nlw-spacetime-logo.svg';
@@ -9,12 +11,42 @@ import Stripes from './assets/stripes.svg';
 
 const StyledStripes = styled(Stripes)
 
+const discovery = {
+  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+  tokenEndpoint: 'https://github.com/login/oauth/access_token',
+  revocationEndpoint: 'https://github.com/settings/connections/applications/4d14945f11b3eed9b317',
+};
+
 export default function App() {
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
     BaiJamjuree_700Bold,
   })
+
+  const [request, response, signInWithGithub] = useAuthRequest(
+    {
+      clientId: '4d14945f11b3eed9b317',
+      scopes: ['identity'],
+      redirectUri: makeRedirectUri({
+        scheme: 'nlwspacetime'
+      }),
+    },
+    discovery
+  );
+
+  useEffect(() => {
+    console.log(makeRedirectUri({
+      scheme: 'nlwspacetime'
+    }));
+    
+
+    if (response?.type === 'success') {
+      const { code } = response.params;
+      console.log(code);
+      
+    }
+  }, [response]);
 
   if (!hasLoadedFonts) {
     return null
@@ -32,7 +64,7 @@ export default function App() {
           <Text className='text-center font-body text-base leading-relaxed text-gray-100'>Colecione momentos marcantes da sua jornada e compartilhe (se quiser) com o mundo!</Text>
         </View>
 
-        <TouchableOpacity activeOpacity={0.7} className='rounded-full bg-green-500 px-5 py-3'>
+        <TouchableOpacity activeOpacity={0.7} className='rounded-full bg-green-500 px-5 py-3' onPress={() => signInWithGithub}>
           <Text className='font-alt text-sm uppercase text-black'>Cadastrar lembrança</Text>
         </TouchableOpacity>
       </View>
--- mobile/README.md
+++ mobile/README.md
@@ -6,3 +6,4 @@
 >- Instale os pacotes com `npm install`.
 >- Faça uma copia do arquivo `.env.example` para `.env` e preencha corretamente.
 >- Execute `npm run start` para iniciar o servidor do emulador que voce decidir utilizar (**Lembrando que caso utilize o _Expo_ é necessário ter uma conta do Expo. Caso utiliza o _Android Studio_ é necessário instalar o ambiente de desenvolvimento**).
+>- Para iniciar o emulador limpando o cache execute `npm run start --clear`.
